5de7d56b98f11fb07c8fe8596e62b65345109db2
TImothy Spriegade <timothyspriegade@outlook.de>
2024-10-03 10:10:19 +0200

    make sure that it is in the correct directory

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Ensure the script is run as root
 if [ "$EUID" -ne 0 ]; then
-echo "Please run as root"
-exit 1
+    echo "Please run as root"
+    exit 1
 fi
 
-#initiate common scripts distro independent
+# Change to the directory where the script is located
+cd "$(dirname "$0")" || exit 1
+echo "Current directory: $(pwd)"
+
+# Initiate common scripts distro independent
 chmod +x ./scripts/init_common.sh
 if [ -x ./scripts/init_common.sh ]; then
     ./scripts/init_common.sh
@@ -13,14 +18,12 @@ fi
 
 # Detect the OS distribution and set the DISTRO environment variable
 if [ -f /etc/arch-release ]; then
-  chmod +x ./scripts/init_arch.sh
- ./scripts/init_arch.sh || exit 1
+    chmod +x ./scripts/init_arch.sh
+    ./scripts/init_arch.sh || exit 1
 elif [ -f /etc/lsb-release ]; then
-  chmod +x ./scripts/debian/init_debian.sh
-  ./scripts/debian/init_debian.sh || exit 1
+    chmod +x ./scripts/debian/init_debian.sh
+    ./scripts/debian/init_debian.sh || exit 1
 else
-echo "Unsupported Linux distribution"
-exit 1
+    echo "Unsupported Linux distribution"
+    exit 1
 fi
-
-
