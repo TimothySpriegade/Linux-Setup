2d48ad50a823cdaae0f3762e1e508bdc7fab9498
TImothy Spriegade <timothyspriegade@outlook.de>
2024-10-03 11:46:00 +0200

    bash setup

--- scripts/common/bash_setup.sh
+++ scripts/common/bash_setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ## synth-shell - beutiful shell prompt
-git clone --recursive https://github.com/andresgongora/synth-shell.git
-chmod +x synth-shell/setup.sh
-cd synth-shell || exit 1
-./setup.sh
+echo "Installing synth-shell-prompt"
+git clone --recursive https://github.com/andresgongora/synth-shell-prompt.git
+synth-shell-prompt/setup.sh
+sudo apt install fonts-powerline
+. ~/.config/synth-shell/synth-shell-prompt.sh
+echo ". ~/.config/synth-shell/synth-shell-prompt.sh" >> ~/.bashrc
 
 # shellcheck disable=SC2164
 cd ~
 
-# shellcheck disable=SC1090
-source ~/.bashrc
-
 ## scm_breeze - git shortcuts
-#git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
-#~/.scm_breeze/install.sh || exit 1
+git clone https://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
+~/.scm_breeze/install.sh || exit 1
+
 ## shellcheck disable=SC1090
-#source ~/.bashrc
+source ~/.bashrc
 
